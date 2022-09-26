8e6818cc1a4be04af74d4aaff118b69382bc4374
jeonghoon <shb03323@naver.com>
2022-09-26 21:47:44 +0900

    [Init] initiation typescript

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = require('./src/router/router');
-var app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/', router);
-app.listen(3000, function () {
-    console.log('Connected port 3000');
-});

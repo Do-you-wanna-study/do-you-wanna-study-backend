674a8f56a7bdaf1d89747e1a68557b152f26082e
jeonghoon <shb03323@naver.com>
2022-10-16 19:13:30 +0900

    [Feat] add main index file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,39 @@
-import express from 'express'
-import router from './router'
+import express, { Request, Response, NextFunction } from 'express';
+import router from './router';
+import dotenv from 'dotenv';
+const app = express();
 
-var app =express();
+dotenv.config();
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use('/', router);
+app.use(router);
 
+interface ErrorType {
+  message: string;
+  status: number;
+}
 
-app.listen(3000, () => {
-	console.log('Connected port 3000');
-})
+app.use(function (err: ErrorType, req: Request, res: Response, next: NextFunction) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'production' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app
+  .listen(config.port, () => {
+    console.log(`
+    ################################################
+        ☁️  Deartoday Server listening on port ☁️
+    ################################################
+  `);
+  })
+  .on('error', (err) => {
+    console.error(err);
+    process.exit(1);
+  });
+
+  export default app;

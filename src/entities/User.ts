aec58904cc0eb9b59f8c6bab7eb37dfc78608ad5
jeonghoon <shb03323@naver.com>
2022-10-16 22:13:54 +0900

    [Feat] 초기 User Entity 제작

---
+++ src/entities/User.ts
@@ -0,0 +1,27 @@
+import { BaseEntity, Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('user')
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('increment')
+  public id: number;
+
+  @Column({ nullable: false, length: 100 })
+  email: string;
+
+  @Column({ nullable: false, length: 6 })
+  nickname: string;
+
+  @Column({ nullable: false, length: 255 })
+  password: string;
+
+  @Column({ name: 'is_deleted', width: 1, default: false })
+  isDeleted: boolean;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  
+}

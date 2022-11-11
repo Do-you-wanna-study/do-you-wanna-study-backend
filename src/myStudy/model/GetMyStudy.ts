b6ad7ebe91adab3db70efd17d509ab705f0f3a28
mihnhyuk <mihnhyuk@naver.com>
2022-11-11 21:42:04 +0900

    [Feat] Mystudy

--- src/myStudy/model/GetMyStudy.ts
+++ src/myStudy/model/GetMyStudy.ts
@@ -1,16 +1,11 @@
-import { StudyGroup } from "../../entities/StudyGroup"
-import { StudyGroupToUser } from "../../entities/StudyGroupToUser";
-import { User } from "../../entities/User";
 import { AppDataSource } from "../../loaders/db"
 
 
 export default async (userId : number) => {
-	const studyGroupRepository = AppDataSource.getRepository(StudyGroup);
-	const result =
-	await studyGroupRepository.createQueryBuilder("study_group")
-	.leftJoinAndSelect("study_group.users", "user")
-	.where("user.id = :id", {id: userId})
+	return AppDataSource
+	.getRepository("study_group_to_user")
+	.createQueryBuilder("study_group_to_user")
+	.leftJoinAndSelect("study_group_to_user.studyGroup", "study_group")
+	.leftJoinAndSelect("study_group_to_user.user", "user")
 	.getMany()
-
-	console.log(result)
 }

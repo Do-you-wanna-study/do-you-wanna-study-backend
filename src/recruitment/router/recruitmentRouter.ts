8c2215ab30007c415c6bcb5e7a6fb86a52eac28c
mihnhyuk <mihnhyuk@naver.com>
2022-11-15 22:01:46 +0900

    [Add] recruitment data에 user 추가 select문 수정 필요할듯

--- src/recruitment/router/recruitmentRouter.ts
+++ src/recruitment/router/recruitmentRouter.ts
@@ -3,11 +3,10 @@ import { RecruitmentController, RecruitmentPostingController } from '../controll
 import isLogin from '../../middleware/AuthCheck';
 
 const router: Router = Router({mergeParams: true});
-// 로그인 정보 있어야하는 것들 2번째 파라미터에 집어 넣으면 됨
+
 router.get('/', RecruitmentController);
 router.get(['/:communityID', '/:communityID/:filter'] , RecruitmentController);
-// router.get('/:communityID/proc', RecruitmentController.procStudy);
-// router.get('/:communityID/done', RecruitmentController.doneStudy);
+
 
 router.post('/', RecruitmentPostingController)
 

3496aff47bead212212978c111acd0444788c921
mihnhyuk <mihnhyuk@naver.com>
2022-12-05 23:14:59 +0900

    [Chore] 안쓰는 로그인 페이지 삭제

--- src/auth/router/authRouter.ts
+++ src/auth/router/authRouter.ts
@@ -1,12 +1,10 @@
 import express from 'express';
-import {loginPage} from '../controller'
 import {loginProcess} from '../controller'
 import { logout } from '../controller';
 import {signUpProcess} from '../controller';
 
 const router : express.Router = express.Router({mergeParams : true});
 
-router.get('/login', loginPage);
 router.post('/login', loginProcess)
 router.get('/logout', logout)
 router.post('/signup', signUpProcess)

b02bf53c4fc840810034368191442f5b9b17b277
mihnhyuk <mihnhyuk@naver.com>
2022-12-05 11:28:33 +0900

    [Bugfix] 댓글 작성 버그 수정

--- src/recruitment/controller/RecruitmentCommentPostController.ts
+++ src/recruitment/controller/RecruitmentCommentPostController.ts
@@ -4,12 +4,12 @@ import { RecruitmentCommentPostService } from '../service';
 
 export default async (req : Request, res : Response) => {
 	
-	if (req.query.recruitmentId === undefined || req.body.content === undefined){
+	if (req.query.id === undefined || req.body.content === undefined){
 		res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, "BAD Requeset, no params"))
 		return
 	}
 	else{
-		const recruitmentId : number = parseInt(req.query.recruitmentId.toString())
+		const recruitmentId : number = parseInt(req.query.id.toString())
 		//@ts-ignore
 		const userId : number = parseInt(req.body.userId.toString())
 		RecruitmentCommentPostService(recruitmentId, userId, req.body.content)

b772a89d90b5ecfdaed3e4c1579e1e4802b525c8
mihnhyuk <mihnhyuk@naver.com>
2022-12-09 01:57:53 +0900

    [Feat] 마이페이지 평점 자릿수 고정

--- src/myPage/service/MyPageService.ts
+++ src/myPage/service/MyPageService.ts
@@ -7,15 +7,16 @@ export default async (userId : number) => {
 	const doneStudy = await GetDoneStudy(userId)
 	// console.log(grade)
 	// console.log(myApply)
-	console.log(doneStudy)
+	// console.log(doneStudy)
 	const avgGrade = {
-		skill: parseFloat(grade.skill_sum) / parseFloat(grade.skill_count),
-		kindness: parseFloat(grade.kindness_sum) / parseFloat(grade.kindness_count),
-		sincerity: parseFloat(grade.sincerity_sum) / parseFloat(grade.sincerity_count),
-		helpful: parseFloat(grade.helpful_sum) / parseFloat(grade.helpful_count),
+		skill: Number((parseFloat(grade.skill_sum) / parseFloat(grade.skill_count)).toFixed(1)),
+		kindness: Number((parseFloat(grade.kindness_sum) / parseFloat(grade.kindness_count)).toFixed(1)),
+		sincerity: Number((parseFloat(grade.sincerity_sum) / parseFloat(grade.sincerity_count)).toFixed(1)),
+		helpful: Number((parseFloat(grade.helpful_sum) / parseFloat(grade.helpful_count)).toFixed(1)),
 	}
 	let sum = avgGrade.skill + avgGrade.kindness + avgGrade.sincerity + avgGrade.helpful
 	sum /= 4
+	sum = Number(sum.toFixed(1))
 	Object.assign(avgGrade, {total: sum})
 	return util.success(statusCode.OK, "mypage data", {grade: avgGrade, apply: myApply, doneStudy: doneStudy})
 }

01f2f6893c1d3c2f3d2279a22804054d178abef3
mihnhyuk <mihnhyuk@naver.com>
2022-12-08 21:57:49 +0900

    [Feat] 회고 작성post

--- src/entities/StudyRetrospect.ts
+++ src/entities/StudyRetrospect.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { StudyGroup } from './StudyGroup';
 import { User } from './User';
 
@@ -14,11 +14,13 @@ export class StudyRetrospect {
     nullable: false,
     lazy: true,
   })
+  @JoinColumn({name:"study_group_id"})
   studyGroup: StudyGroup;
 
   @ManyToOne(() => User, (User) => User.studyRetrospectList, {
     nullable: false,
     lazy: true,
   })
+  @JoinColumn({name:"user_id"})
   user: User;
 }
--- src/myStudy/model/Review.ts
+++ src/myStudy/model/Review.ts
@@ -1,21 +1,49 @@
 import { StudyGroup } from "../../entities/StudyGroup"
 import { StudyRetrospect } from "../../entities/StudyRetrospect"
 import { User } from "../../entities/User"
+import { UserEvaluation } from "../../entities/UserEvaluation"
 import { AppDataSource } from "../../loaders/db"
 
-export default async (inputData: { userId: number, studyId: number, content: string, evaluates: any }) => {
+export default async (inputData: any) => {
 	const {userId, studyId, content, evaluates} = inputData
 	
-	const studyGroup = AppDataSource
-	.getRepository(StudyGroup)
-	.findOne({where:{id : studyId}})
-	const user = AppDataSource
-	.getRepository(User)
-	.findOne({where:{id:userId}})
+	// const studyGroup = await AppDataSource
+	// .getRepository(StudyGroup)
+	// .findOne({where:{id : studyId}})
+	// const user = await AppDataSource
+	// .getRepository(User)
+	// .findOne({where:{id:userId}})
+	
+	const studyGroup = new StudyGroup()
+	studyGroup.id = studyId
+	// await AppDataSource.manager.save(studyGroup)
+
+	const user = new User()
+	user.id = userId
+	// await AppDataSource.manager.save(user)
+
 	const newReview = new StudyRetrospect()
 	newReview.content = content
-	// newReview.studyGroup = await studyGroup
-	// newReview.user = await user
-	
-	// AppDataSource.manager.save(newReview)
+	if (( studyGroup) === null || ( user) === null){
+		return -1
+	}
+	newReview.studyGroup = studyGroup
+	newReview.user = user
+	console.log(newReview)
+	AppDataSource.manager.save(newReview)
+
+	for (let i = 0 ; i < evaluates.length ; i++){
+		const toUser = new User()
+		toUser.id = parseInt(evaluates[i].userId)
+		const newEvaluate = new UserEvaluation()
+		newEvaluate.content = evaluates[i].content
+		newEvaluate.toUser = toUser
+		newEvaluate.fromUser = user
+		newEvaluate.skill = parseInt(evaluates[i].skill)
+		newEvaluate.kindness = parseInt(evaluates[i].kindness)
+		newEvaluate.sincerity = parseInt(evaluates[i].sincerity)
+		newEvaluate.helpful = parseInt(evaluates[i].helpful)
+		AppDataSource.manager.save(newEvaluate)
+	}
+	return	
 }

b00c3154b8aa737500689eff90a1f18da08b9878
mihnhyuk <mihnhyuk@naver.com>
2022-12-08 14:22:28 +0900

    [Bugfix] 스터디 중복지원

--- src/recruitment/controller/RecruitmentApplyController.ts
+++ src/recruitment/controller/RecruitmentApplyController.ts
@@ -14,8 +14,11 @@ export default async (req : Request, res : Response) => {
 		const userId : number = req.body.userId
 		const recruitmentId : number = parseInt(req.query.id.toString())
 
-		RecruitmentApplyService(userId, recruitmentId, req.body.description)
-		res.status(statusCode.OK).send(util.success(statusCode.OK, "apply success"))
+		if (await RecruitmentApplyService(userId, recruitmentId, req.body.description)){
+			res.status(statusCode.OK).send(util.success(statusCode.OK, "apply success"))
+		}else{
+			res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, "duplicated user input"))
+		}
 		return
 	}
 }
